eb2c38c8705fc53455c992b4476f595a2e829013
Rupesh Singh <46321467+1RupeshSingh@users.noreply.github.com>
2021-07-28 01:20:23 +0530

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,28 @@
+//Simple Animated car project
+m=0;
+var y;
+function start()
+{
+
+    y=setInterval(run,80);
+
+    function run()
+    {
+        if(m==1300)
+        {
+          clearInterval(y);
+          m=0
+        }
+        else
+        {
+        m+=5;
+        var x=document.getElementById("img");
+        x.style.marginLeft=m+'px';
+        }
+    }
+}
+
+function stop()
+{
+    clearInterval(y)
+}
